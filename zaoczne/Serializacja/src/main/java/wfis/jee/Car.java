e32365658fea467e4341425952dba863f1c122df
kpodlaski <podlaski@uni.lodz.pl>
2024-10-20 17:50:12 +0200

    serializacja json

---
+++ zaoczne/Serializacja/src/main/java/wfis/jee/Car.java
@@ -0,0 +1,49 @@
+package wfis.jee;
+
+public class Car {
+    private int ID;
+    private String brand;
+    private int year;
+    private String plate;
+
+    public Car(){}
+
+    public Car(int ID, String brand, int year, String plate) {
+        this.ID = ID;
+        this.brand = brand;
+        this.year = year;
+        this.plate = plate;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getPlate() {
+        return plate;
+    }
+
+    public void setPlate(String plate) {
+        this.plate = plate;
+    }
+}
---
+++ zaoczne/Serializacja/src/main/java/wfis/jee/CarRegistry.java
@@ -0,0 +1,55 @@
+package wfis.jee;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarRegistry {
+
+    public static void main(String[] args) {
+        serialize();
+        deserialize();
+    }
+
+    public static void serialize(){
+        Car c = new Car(1,"Volvo",2005, "EL12345");
+        List<Car> cars = new ArrayList<>();
+        cars.add(c);
+        cars.add(new Car(2,"Syrenka",1974,"LWX1234"));
+        ObjectMapper om = new ObjectMapper();
+        om.enable(SerializationFeature.INDENT_OUTPUT);
+        try {
+            String json = om.writeValueAsString(c);
+            System.out.println(json);
+            json = om.writeValueAsString(cars);
+            System.out.println(json);
+            om.writeValue(new File("cars.json"),cars);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void deserialize(){
+        ObjectMapper om = new ObjectMapper();
+        om.enable(SerializationFeature.INDENT_OUTPUT);
+        try {
+            Car[] cars = om.readValue(new File("cars.json"),Car[].class);
+            for (Car c :cars)
+            {
+                System.out.println(c.getBrand()+" "+c.getPlate());
+            }
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
