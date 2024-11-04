f9ab531fb7f67c085e683461d92280fc9d582ebe
kpodlaski <podlaski@uni.lodz.pl>
2024-11-04 13:21:25 +0100

    dao szablon

---
+++ dzienne/BazaDanych/src/main/java/dao/jdbc/PositionDAO.java
@@ -0,0 +1,43 @@
+package dao.jdbc;
+
+import dao.PositionDAO;
+import model.Position;
+
+import java.util.List;
+
+public class PositionDAO implements dao.PositionDAO{
+    @Override
+    public List<Position> getAllPositions() {
+        return null;
+    }
+
+    @Override
+    public Position getPosition(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Position> getPositionByName(String name) {
+        return null;
+    }
+
+    @Override
+    public int update(Position p) {
+        return 0;
+    }
+
+    @Override
+    public int insert(Position p) {
+        return 0;
+    }
+
+    @Override
+    public int delete(Position p) {
+        return 0;
+    }
+
+    @Override
+    public int deletePosition(int id) {
+        return 0;
+    }
+}
